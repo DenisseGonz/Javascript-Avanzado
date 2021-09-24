4ecec9cc0ccf1e07b1be4773bd4abe5f119b14f0
Denisse Gonzalez <denisse.gonz.r@gmail.com>
2021-09-23 23:12:59 -0500

    clase 23 de septiembre

---
+++ 12-arrow-function.js
@@ -0,0 +1,11 @@
+/*Funciones de Flecha */
+
+/*function suma (n1,n2){
+    return n1+n2;
+}*/
+
+const suma = (num1,num2) =>{
+    return num1+num2;
+}
+console.log(suma(7,8));
+document.write(`<p>${suma(7,8)}</p>`)
